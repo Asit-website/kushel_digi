972dfc97052e6d883986775ce193540e661d6df7
Asit-website <asitmandal492@gmail.com>
2023-03-21 03:46:10 +0530

    this

--- src/components/Cards/PortCard.jsx
+++ src/components/Cards/PortCard.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
-const PortCard = () => {
+const PortCard = (props) => {
     return (
         <>
             <div className="port-card mx-4 mb-7">
                 <div className="port-card1 relative">
-                    <img className='h-full w-full' src="/static/images/a9.png" alt="" />
+                    <img className='h-full w-full' src={props.pads} alt="" />
                     <button className='absolute'>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
--- src/components/Cards/ServiceCard.jsx
+++ src/components/Cards/ServiceCard.jsx
@@ -1,39 +1,39 @@
 import React from 'react'
 
-const ServiceCard = () => {
+const ServiceCard = (props) => {
     return (
         <>
             <div className="serviceCard p-5 bg-white mx-2">
                 <div className="s-card">
                     <div className="s-card0">
                         <div className="s-card1 flex items-center mb-6">
-                            <img src="/static/images/a3.png" alt="" />
-                            <h5 className="text-2xl font-bold ml-2">App Development</h5>
+                            <img src={props.app1} alt="" />
+                            <h5 className="text-2xl  ml-2">{props.dev} <br />{props.dev1}</h5>
                         </div>
                         <div className="s-card1 flex items-center mb-3">
-                            <img src="/static/images/a4.png" alt="" />
-                            <p className="ml-2">Android App</p>
+                            <img src={props.android} alt="" />
+                            <p className="ml-2">{props.a}</p>
                         </div>
                         <div className="s-card1 flex items-center mb-3">
-                            <img src="/static/images/a4.png" alt="" />
-                            <p className="ml-2">IOS App</p>
+                            <img src={props.apple} alt="" />
+                            <p className="ml-2">{props.b}</p>
                         </div>
                         <div className="s-card1 flex items-center mb-3">
-                            <img src="/static/images/a4.png" alt="" />
-                            <p className="ml-2">Flutter</p>
+                            <img src={props.fluter} alt="" />
+                            <p className="ml-2">{props.c}</p>
                         </div>
                         <div className="s-card1 flex items-center mb-3">
-                            <img src="/static/images/a4.png" alt="" />
-                            <p className="ml-2">React Native</p>
+                            <img src={props.react} alt="" />
+                            <p className="ml-2">{props.d}</p>
                         </div>
                         <div className="s-card1 flex items-center mb-3">
-                            <img src="/static/images/a4.png" alt="" />
-                            <p className="ml-2">Kotlin</p>
+                            <img src={props.kotlin} alt="" />
+                            <p className="ml-2">{props.e}</p>
                         </div>
                     </div>
                     <div className="s-card2 mt-16">
                         <button className="btn btn1 btn2">
-                            <span className='mr-2'>Contact Us</span>
+                            <span className='mr-2'>Read More</span>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
                             </svg>
